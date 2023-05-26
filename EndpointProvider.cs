e2e2a9bafcb5ea29b3b1397b1bb430d796a23f6c
David Bouška <bouskdav@hotmail.com>
2023-05-26 18:55:42 +0200

    png endpoints

--- EndpointProvider.cs
+++ EndpointProvider.cs
@@ -18,6 +18,16 @@ namespace Payments.BankTransfer
                  new { controller = "PaymentBankTransfer", action = "PaymentInstructions", area = "" }
             );
 
+            endpointRouteBuilder.MapControllerRoute("Plugin.PaymentBankTransferCodeFile",
+                 $"{BankTransferPaymentDefaults.PaymentCodeUrl}/{{orderId}}.png",
+                 new { controller = "PaymentBankTransfer", action = "PaymentCode", area = "" }
+            );
+
+            endpointRouteBuilder.MapControllerRoute("Plugin.PaymentBankTransferCodeByNumberFile",
+                 $"{BankTransferPaymentDefaults.PaymentCodeByNumberUrl}/{{orderNumber}}.png",
+                 new { controller = "PaymentBankTransfer", action = "PaymentCodeByNumber", area = "" }
+            );
+
             endpointRouteBuilder.MapControllerRoute("Plugin.PaymentBankTransferCode",
                  $"{BankTransferPaymentDefaults.PaymentCodeUrl}/{{orderId}}",
                  new { controller = "PaymentBankTransfer", action = "PaymentCode", area = "" }
