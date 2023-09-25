2327d006af8a1a14cdaa092dd38e82adb3c3d47e
David Bouška <bouskdav@hotmail.com>
2023-09-25 17:48:11 +0200

    Update Manifest.cs

--- Manifest.cs
+++ Manifest.cs
@@ -6,7 +6,6 @@ using Payments.BankTransfer;
     FriendlyName = "Bank transfer with QR payment",
     Group = "Payment methods",
     SystemName = BankTransferPaymentDefaults.ProviderSystemName,
-    SupportedVersion = GrandVersion.SupportedPluginVersion,
     Author = "Laguna Solutions",
     Version = "2.00"
 )]
